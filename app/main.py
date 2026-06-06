fe95d02ea1b018f23653fcf88023653dbf6e7214
Andrés Tapia <andres.tapia@zentracore.net>
2026-06-06 10:32:44 -0500

    feat: add health structure

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.routes.health_routes import health_router
+
+app = FastAPI(
+    title="CTT Course",
+    description="CTT Course of FISEI",
+    version="1.0.0"
+)
+
+
+app.include_router(router=health_router)
