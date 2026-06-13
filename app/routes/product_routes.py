7edd5cf16715f68ce40df8cf90a9b0bd9dcac61f
Andres Tapia <andrestgonza@gmail.com>
2026-06-13 10:30:07 -0500

    feat: add routes dependency injections

--- app/routes/product_routes.py
+++ app/routes/product_routes.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
+from app.core.database import get_db
 from app.schemas.product_models import ProductCreate, ProductResponse, ProductUpdate
 from app.services.product_services import ProductServices
 
@@ -7,21 +9,21 @@ product_router = APIRouter(prefix="/products", tags=["Products"])
 product_service = ProductServices()
 
 @product_router.get("/")
-def get_all_products_api() -> list[ProductResponse]:
-    pass
+def get_all_products_api(db: Session = Depends(get_db)) -> list[ProductResponse]:
+    return product_service.get_all_products(db=db)
 
 @product_router.get("/{product_id}")
-def get_product_api(product_id: int) -> ProductResponse | None:
-    pass
+def get_product_api(product_id: int, db: Session = Depends(get_db)) -> ProductResponse | None:
+    return product_service.get_product(db=db, product_id=product_id)
 
 @product_router.post("/")
-def create_product_api (product_data: ProductCreate) -> ProductResponse:
-    pass
+def create_product_api (product_data: ProductCreate, db: Session = Depends(get_db)) -> ProductResponse:
+    return product_service.create_product(product_data=product_data, db=db)
 
 @product_router.put("/{product_id}")
-def update_product_api(product_id: int, product_data: ProductUpdate) -> ProductResponse | None:
-    pass
+def update_product_api(product_id: int, product_data: ProductUpdate, db: Session = Depends(get_db)) -> ProductResponse | None:
+    return product_service.update_product(db=db, product_id=product_id, product_data=product_data)
 
 @product_router.delete("/{product_id}")
-def delete_product_api(product_id: int) -> bool:
-    pass
+def delete_product_api(product_id: int, db: Session = Depends(get_db)) -> bool:
+    return product_service.delete_product(product_id=product_id, db=db)
